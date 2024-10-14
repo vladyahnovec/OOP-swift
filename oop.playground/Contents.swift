bfbf456d552228c1a5834090f4dd81b861252cb3
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-14 13:52:29 +0300

    Encapsulation example

--- oop.playground/Contents.swift
+++ oop.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Инкапсуляция
+/*Сокрытие внутренних данных компонента и деталей его реализации от других компонентов приложения и предоставление набора методов для взаимодействия с ним(API). Инкапсуляция реализуется с помощью модификаторов доступа(public, private)*/
+class Car {
+    public var weight : Int = 0
+    private var maxSpeed : Int = 0
+    
+    func getMaxSpeed() -> Int {
+        return self.maxSpeed
+    }
+    func setMaxSpeed(mSpeed: Int) {
+        self.maxSpeed = mSpeed
+    }
+}
+
+var firstCar = Car()
+print(firstCar.weight, firstCar.getMaxSpeed())
+firstCar.setMaxSpeed(mSpeed: 100)
+firstCar.weight = 100
+print(firstCar.weight, firstCar.getMaxSpeed())
