df211735d1f513e69f1ae301bf07c451c08be560
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-14 14:09:56 +0300

    Inheritance example

--- oop.playground/Contents.swift
+++ oop.playground/Contents.swift
@@ -19,3 +19,41 @@ print(firstCar.weight, firstCar.getMaxSpeed())
 firstCar.setMaxSpeed(mSpeed: 100)
 firstCar.weight = 100
 print(firstCar.weight, firstCar.getMaxSpeed())
+
+//Наследование
+/*Позволяет описать новый класс на основе существующего (родительского). При этом свойства и методы родительского класса взаимствует новый класс.*/
+class Person {
+    var name: String
+    var age: Int
+    var weight: Double
+    
+    init(name: String, age: Int, weight: Double) {
+        self.name = name
+        self.age = age
+        self.weight = weight
+    }
+    
+    func greeting() {
+        print("Привет, я человек, меня зовут \(self.name) мне \(self.age) лет и мой вес \(self.weight)кг.")
+    }
+}
+
+class Teacher: Person {
+    var experience: Int
+
+    init(name: String, age: Int, weight: Double, experience: Int) {
+        self.experience = experience
+        super.init(name: name, age: age, weight: weight)
+    }
+    
+    
+    override func greeting() {
+        print("Привет, я учитель, меня зовут \(self.name) мне \(self.age) лет,  мой вес \(self.weight)кг, мой стаж \(self.experience)лет.")
+    }
+}
+
+var firstPerson = Person(name: "Vlad", age: 19, weight: 70)
+var firstTeacher = Teacher(name: "Svetlana", age: 45, weight: 75, experience: 25)
+firstPerson.greeting()
+firstTeacher.greeting()
+
